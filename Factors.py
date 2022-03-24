76e31b806911fe3746c71eec0d8b4b9224a772b0
vasanth375 <vasanthnimmala69@outlook.com>
2022-03-24 20:11:00 +0530

    Factor and Perfect

--- Factors.py
+++ Factors.py
@@ -1,8 +1,14 @@
-n=33
-factors=[]
-for i in range(1,n+1):
-    for j in range(1, n+1):
-        if i*j == n:
-            factors.append(j)
-
-print(sorted(factors))
+for k in range(3):
+    n=int(input("Enter number"))
+    factors=[]
+    for i in range(1,n+1):
+        for j in range(1, n+1):
+            if i*j == n:
+                factors.append(j)
+    if n in factors:
+        factors.remove(n)
+    print(sorted(factors))
+    if sum(factors) == n:
+        print("It is perfect number")
+    else:
+        print("Not a perfect number")
