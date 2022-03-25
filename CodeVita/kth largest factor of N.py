de316a050b3dbcc2bdb46825d7e16548d5d6eda3
vasanth375 <vasanthnimmala69@outlook.com>
2022-03-25 16:34:57 +0530

    CodeVita

--- CodeVita/kth largest factor of N.py
+++ CodeVita/kth largest factor of N.py
@@ -2,9 +2,10 @@ N,k=list(map(int, input().split(',')))
 # need to find factor
 # index number need to find value after finding factors of N
 factor=[]
-
+# finding factors
 for i in range(1, N+1):
     if N%i == 0:factor.append(i)
 
+# if k > number of factors return 1
 if k < len(factor):print(factor[k])
 else:print(1)
