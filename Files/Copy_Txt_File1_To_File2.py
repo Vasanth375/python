3b7759b2db8c6ca177496d225e213467bd3acaa7
Vasanth N <vasanthnimmala69@outlook.com>
2022-05-29 22:32:54 +0530

    Transferring of Text 1:1

--- Files/Copy_Txt_File1_To_File2.py
+++ Files/Copy_Txt_File1_To_File2.py
@@ -1,5 +1,10 @@
-with open('python\Files\Hello.txt','r+') as fp, open('python\Files\File2.txt','r+') as fp2:
-    for i in fp:
-        fp2.write(i)
-    print(fp.read())
-    print(fp2.read())
+#Method 1
+# with open('Files\Hello.txt','r') as fp, open('Files\File2txt','w') as fp2:
+#     for i in fp:
+#         fp2.write(i)
+
+# method 2
+file1 = open('Files\Hello.txt','r') 
+file2 = open('Files\File2txt', 'w')
+for i in file1:
+    file2.write(i)
