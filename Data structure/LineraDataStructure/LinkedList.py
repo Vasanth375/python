8c0e991f09819dc1269dda4e7b01f5573155b969
Vasanth N <vasanthnimmala69@outlook.com>
2022-08-16 13:48:53 +0530

    Single LinkedList

--- Data structure/LineraDataStructure/LinkedList.py
+++ Data structure/LineraDataStructure/LinkedList.py
@@ -1,73 +1,50 @@
-# class Node:
-#     def __init__(self,data):
-#         self.data = data
-#         self.next = None 
-
-# class Solution: 
-#     def display(self,head):
-#         current = head
-#         while current:
-#             print(current.data,end=' ')
-#             current = current.next
-
-#     def insert(self,head,data): 
-#         temp = Node(data)
-#         if head is None:
-#             head = temp
-#             return head
-#         current = head
-#         while current.next is not None:
-#             current = current.next
-#         current.next = temp
-#         return head 
-
-# mylist= Solution()
-# T=2
-# head=None
-# data=1
-# for i in range(T):
-#     head=mylist.insert(head,data)  
-#     data=data+1 
-# mylist.display(head)
 class Node:
- def __init__(self, val):
-    self.data = val
-    self.next = None
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
 
- def getData(self):
-    return self.data
+class Solution: 
+    def display(self,head):
+        '''display method will traverse through linked list and display each data in it'''
 
- def getNext(self):
-    return self.next
+        current = head  # current points to head
 
- def setData(self, val):
-    self.data = val
+        while current:  # checking if current is pointing to None or not
+            print(current.data,end=' ')
+            current = current.next  # current is now pointing to next node
 
- def setNext(self, val):
-    self.next = val
+    def insert(self,head,data): 
+        '''Insert method used to creating new data 
+            to existing data'''
+            
+        temp = Node(data)   # creating new node
+        if head is None:    # this block of code is executed only once in the whole life of the program
+                            # checks if head is pointing to None or not
+            head = temp     # now created head points to new node
+            return head     
+        
+        # if the above if condition goes False interpreter comes below 
+        current = head      # now head points to current
+        while current.next is not None:     # checking if the current's next is not None
+            current = current.next    
+        current.next = temp     # current.next is now pointing to newly created node
+        return head 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+mylist= Solution()
+# T=int(input("Enter the nodes you want to Insert: "))
+T = 3
+head=None
+data=1
+for i in range(T):
+    head=mylist.insert(head,data)  
+    data=data+1 
+mylist.display(head)
 
-    def isEmpty(self):
-        """Check if the list is empty"""
-        return self.head is None
 
-    def add(self, item):
-        """Add the item to the list"""
-        new_node = Node(item)
-        new_node.setNext(self.head)
-        self.head = new_node
+# l=[1,2,3]
+# # print(l.__len__())
+# # print(dir(l))
 
-    def printList(self):
-        """Print the list"""
-        current = self.head
-        while current is not None:
-            print(current.getData())
-            current = current.getNext()
+# di = {'a':1, 'b':2}
+# # print(dir(di))
 
-l1=LinkedList()
-l1.add(10)
-l1.add(20)
-l1.printList()
