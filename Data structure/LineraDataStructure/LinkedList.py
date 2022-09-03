0c2985b13565377c38a13a00e2d3c3a9779c8721
Vasanth N <vasanthnimmala69@outlook.com>
2022-09-03 13:56:50 +0530

    Python LinkedList

--- Data structure/LineraDataStructure/LinkedList.py
+++ Data structure/LineraDataStructure/LinkedList.py
@@ -4,11 +4,20 @@ class Node:
         self.next = None 
 
 class Solution: 
+
+    def InsertAtFirst(self, head, data):
+        '''Inserting the data at front'''
+        temp = Node(data)   # creating the node
+        temp.next = head    # appending the data linking the temp node main head
+        head = temp         # pointing the head node to created temp node
+        return head         # returing the head node
+
+
     def display(self,head):
         '''display method will traverse through linked list and display each data in it'''
 
         current = head  # current points to head
-
+                
         while current:  # checking if current is pointing to None or not
             print(current.data,end=' ')
             current = current.next  # current is now pointing to next node
@@ -38,13 +47,35 @@ data=1
 for i in range(T):
     head=mylist.insert(head,data)  
     data=data+1 
+mylist.display(head)    ## displaying the data before adding the data at front
+head = mylist.InsertAtFirst(head, 10)
 mylist.display(head)
 
 
-# l=[1,2,3]
-# # print(l.__len__())
-# # print(dir(l))
+# # l=[1,2,3]
+# # # print(l.__len__())
+# # # print(dir(l))
 
-# di = {'a':1, 'b':2}
-# # print(dir(di))
+# # di = {'a':1, 'b':2}
+# # # print(dir(di))
 
+# def shift_list(array, s):
+#     """Shifts the elements of a list to the left or right.
+#     Args:
+#     array - the list to shift
+#     s - the amount to shift the list ('+': right-shift, '-': left-shift)
+#     Returns:
+#     shifted_array - the shifted list
+#     GoalKicker.com – Python® Notes for Professionals 145
+#     """
+#     # calculate actual shift amount (e.g., 11 --> 1 if length of the array is 5)
+#     s %= len(array)
+#     # reverse the shift direction to be more intuitive
+#     s *= -1
+#     # shift array with list slicing
+#     shifted_array = array[s:] + array[:s]
+#     return shifted_array
+    
+# my_array = [1, 2, 3, 4, 5]
+#     # negative numbers
+# print(shift_list(my_array, -7))
